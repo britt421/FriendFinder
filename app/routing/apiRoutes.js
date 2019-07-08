84f89e49b9d1e602be1fd3b009cce433466d1344
Brittany Taylor <hopkins.britt@gmail.com>
2019-07-07 20:28:42 -0500

    added math functions to apiRoutes.js file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,67 @@
-var apiRoutes = {
-    individuals:  "/api/friends"
-};
+//Requiring path and data in friends.js file
+var friends = require("..data/friends.js");
+var path = require("path");
+
+//Routes
+// ================================================
+
+//Exporting functions to send associated HTML pages to corresponding routes
+module.exports = function (app) {
+
+    //Link for User to access API friend data
+    app.get("/api/friends", function(req, res) {
+        res.json(friends);
+    });
 
 
-module.exports = apiRoutes;
+//Method that compares data to make a friend match
+// =================================================
+app.post("/api/friends", function(req, res) {
+    console.log(req.body);
+
+    //Variable to hold the User's score
+    var surveyResult = req.body.scores;
+
+    //Looping through the scores and making each one an integer
+    for (var i = 0; i < surveyResult.length; i++) {
+        surveyResult[i] = parseInt(surveyResult[i]);
+    }
+
+
+    //Variable to store the calculated difference between the User's score and the friends in the array.
+    var totalDiff = 100;
+    
+    //Variable to hold the User's cloest match
+    var friendMatch = 0;
+
+    //Looping through the scores in the friends array and created variable tempDiff to compare scores.
+    for (i = 0; i < friends.length; i++) {
+        var tempDiff = difference(surveyResult, friends[i].scores);
+        console.log("This is the difference between", surveyResult, "and", friends[i].name, friends[i].scores, "=", tempDiff);
+
+    //This is a Bubble Sort to compare tempDiff (the score of a friend) to totalDiff (see above).
+    //The lower the difference the better the match.
+    //If tempDiff < totalDiff then it is the best match so far and the var friendMatch will be updated
+    //with the index # of the best match (i.e. lowest difference in scores).
+
+        if (tempDiff < totalDiff) {
+            totalDiff = tempDiff;
+            friendMatch = i;
+        }
+    }
+
+    //Calculates the difference between the User array and the Friends array.
+
+        function difference(array1, array2) {
+            var amountDiff = 0;
+
+        for (var i = 0; i < array1.length; i++) {
+            amountDiff += Math.abs(array[i] - array2[i]);
+        }
+        return amountDiff;
+        }
+
+    //Sends the Friend Match to survey.html
+    res.send(friends[friendMatch]);
+    });
+}
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,22 @@
+// // Dependencies
+// // ===============================================
+// var path = require("path");
+
+// //Exporting functions that will send the correct HTML page to the corresponding route
+// module.exports = function (app) {
+
+//     //Home page
+//     app.use(function(req, res) {
+//         res.sendFile(path.join(__dirname, "/../public/home.html"));
+//     });
+
+//     //Survey page
+//     app.get("/survey", function(req, res) {
+//         res.sendFile(path.join(__dirname, "/../public/survey.html"));
+//     });
+// };
+
+
 var htmlRoutes = {
     home:  "app/public/home.html",
     survey: "app/public/survey.html"
