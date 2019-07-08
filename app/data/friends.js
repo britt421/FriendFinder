62f1aebd833fdb27e60cfa9a8f42242b629fdfc6
Brittany Taylor <hopkins.britt@gmail.com>
2019-07-07 19:38:05 -0500

    added img urls

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,13 @@
 var friends = [
     {
         "name":"Anna Smith",
-        "photo":"https://xyz.jpg",
+        "photo":"http://ironingboardcollective.files.wordpress.com/2012/01/anna.jpg",
         "scores":[
             5,
             5,
             5,
             5,
-            5,
+            5,  
             5,
             5,
             5,
@@ -17,7 +17,7 @@ var friends = [
     },
     {
         "name":"Lady Mary",
-        "photo":"https://abc.jpg",
+        "photo":"http://d2buyft38glmwk.cloudfront.net/media/images/canonical/mast-da-s5-catchup-mary-looks-ss-01.jpg",
         "scores":[
             3,
             3,
@@ -33,7 +33,7 @@ var friends = [
     },
     {
         "name":"Thomas Barrow",
-        "photo":"https://xyz.jpg",
+        "photo":"https://vignette.wikia.nocookie.net/downtonabbey/images/4/4e/Thomas_barrow_smirk_1_%282%29.jpg/revision/latest?cb=20160101151018",
         "scores":[
             1,
             1,
